969168d060dc4138a80084e5408d1d5c738ce120
Lucia Cerchie <luciacerchie@gmail.com>
2020-07-27 09:39:35 -0700

    405 error this time

--- app.py
+++ app.py
@@ -22,3 +22,14 @@ def return_board():
     board = boggle_game.make_board()
 
     return render_template("board.html", board=board)
+
+
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary."""
+
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
--- static/app.js
+++ static/app.js
@@ -1,6 +1,3 @@
-$(".add-word", this.board).on("submit", this.handleSubmit.bind(this));
-
-
 // async function button() {
 //     let $formcontainer = $("#form-container");
 //     let $form = $("#form")
@@ -21,36 +18,86 @@ $(".add-word", this.board).on("submit", this.handleSubmit.bind(this));
 //^my attempt at step Three, totally missed that I could just grab the word
 
 /* handle submission of word: if unique and valid, score & show */
+class BoggleGame {
+    /* make a new game at this DOM id */
 
-async function handleSubmit(e) {
-    e.preventDefault(); //prevent page from refreshing
-    const $word = $(".words", this.board); //grabs value of word using board for context
-    console.log($word)
-    let word = $word.val(); //grabbing the val with  jQuery
-    if (!word) return;
+    constructor(boardId, secs = 60) {
+        this.secs = secs; // game length
+        this.showTimer();
 
-    if (this.words.has(word)) {
-        this.showMessage(`Already found ${word}`, "err");
-        return;
+        this.score = 0;
+        this.words = new Set();
+        this.board = $("#" + boardId);
+
+        // every 1000 msec, "tick"
+        this.timer = setInterval(this.tick.bind(this), 1000);
+
+        $(".add-word", this.board).on("submit", this.handleSubmit.bind(this));
+    };
+
+    showWord(word) {
+        $(".words", this.board).append($("<li>", {
+            text: word
+        }));
     }
 
-    // check server for validity
-    const resp = await axios.get("/check-word", {
-        params: {
-            word: word
+    /* show score in html */
+
+    showScore() {
+        $(".score", this.board).text(this.score);
+    }
+
+    /* show a status message */
+
+    showMessage(msg, cls) {
+        $(".msg", this.board)
+            .text(msg)
+            .removeClass()
+            .addClass(`msg ${cls}`);
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault(); //prevent page from refreshing
+        const $word = $(".word", this.board); //grabs value of word using board for context
+        console.log($word)
+        let word = $word.val(); //grabbing the val with  jQuery
+        if (!word) return;
+
+        if (this.words.has(word)) {
+            this.showMessage(`Already found ${word}`, "err");
+            return;
         }
-    });
-    if (resp.data.result === "not-word") {
-        this.showMessage(`${word} is not a valid English word`, "err");
-    } else if (resp.data.result === "not-on-board") {
-        this.showMessage(`${word} is not a valid word on this board`, "err");
-    } else {
-        this.showWord(word);
-        this.score += word.length;
-        this.showScore();
-        this.words.add(word);
-        this.showMessage(`Added: ${word}`, "ok");
+
+        // check server for validity
+        const resp = await axios.get("/check-word", {
+            params: {
+                word: word
+            }
+        });
+        if (resp.data.result === "not-word") {
+            this.showMessage(`${word} is not a valid English word`, "err");
+        } else if (resp.data.result === "not-on-board") {
+            this.showMessage(`${word} is not a valid word on this board`, "err");
+        } else {
+            this.showWord(word);
+            this.score += word.length;
+            this.showScore();
+            this.words.add(word);
+            this.showMessage(`Added: ${word}`, "ok");
+        }
+
     }
+    /* end of game: score and update message. */
 
-
+    async scoreGame() {
+        $(".add-word", this.board).hide();
+        const resp = await axios.post("/post-score", {
+            score: this.score
+        });
+        if (resp.data.brokeRecord) {
+            this.showMessage(`New record: ${this.score}`, "ok");
+        } else {
+            this.showMessage(`Final score: ${this.score}`, "ok");
+        }
+    }
 }
